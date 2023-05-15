d1f70f50b4b059007cd2ddb0013d209ceeef4d2d
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2023-05-15 23:08:10 +0200

    isk nmp

--- app/src/main/java/com/example/meme/upload.java
+++ app/src/main/java/com/example/meme/upload.java
@@ -17,7 +17,6 @@ public class upload extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((MainActivity)getActivity()).Portrait();
     }
 
     @Override
